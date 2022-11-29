4c984e633c0b829cbff3f5da0cc22674e830e11e
邹景立 <zoujingli@qq.com>
2022-11-29 15:24:59 +0800

    修改默认规则

--- src/Service.php
+++ src/Service.php
@@ -19,32 +19,15 @@ namespace app\wechat;
 use app\wechat\command\Auto;
 use app\wechat\command\Fans;
 use app\wechat\service\AutoService;
+use think\admin\service\PluginService;
 
 /**
  * 组件注册服务
  * Class Service
  * @package app\wechat
  */
-class Service extends \think\Service
+class Service extends PluginService
 {
-    /**
-     * 指定版本号
-     * @var string
-     */
-    const VERSION = '0.0.1';
-
-    /**
-     * 配置组件服务
-     * @return void
-     */
-    public function boot(): void
-    {
-        $attr = explode('\\', __NAMESPACE__);
-        $addons = $this->app->config->get('app.addons', []);
-        $addons[array_pop($attr)] = __DIR__ . DIRECTORY_SEPARATOR . '@' . join('\\', $attr);
-        $this->app->config->set(['addons' => $addons], 'app');
-    }
-
     /**
      * 注册组件服务
      * @return void
