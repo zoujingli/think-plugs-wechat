343f51d38deee9683f03db1551e395f72621eae7
邹景立 <zoujingli@qq.com>
2022-12-16 12:29:00 +0800

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -41,4 +41,31 @@ class Service extends Plugin
             AutoService::register($openid);
         });
     }
+
+    /**
+     * 增加微信配置
+     * @return array[]
+     */
+    public static function menu(): array
+    {
+        return [
+            [
+                'name' => '微信管理',
+                'subs' => [
+                    ['name' => '微信接口配置', 'icon' => 'layui-icon layui-icon-set', 'node' => 'wechat/config/options'],
+                    ['name' => '微信支付配置', 'icon' => 'layui-icon layui-icon-set', 'node' => 'wechat/config/options'],
+                ]
+            ],
+            [
+                'name' => '微信定制',
+                'subs' => [
+                    ['name' => '微信粉丝管理', 'icon' => 'layui-icon layui-icon-username', 'node' => 'wechat/fans/index'],
+                    ['name' => '微信图文管理', 'icon' => 'layui-icon layui-icon-template-1', 'node' => 'wechat/news/index'],
+                    ['name' => '微信菜单配置', 'icon' => 'layui-icon layui-icon-cellphone', 'node' => 'wechat/menu/index'],
+                    ['name' => '回复规则管理', 'icon' => 'layui-icon layui-icon-engine', 'node' => 'wechat/keys/index'],
+                    ['name' => '关注自动回复', 'icon' => 'layui-icon layui-icon-release', 'node' => 'wechat/auto/index'],
+                ]
+            ]
+        ];
+    }
 }
